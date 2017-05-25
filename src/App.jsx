e4c8ac675482ba1dedc9a191ba8febebc4aedbc7
Mark Winterbottom <mark@londonappdeveloper.com>
2017-05-25 20:26:11 +0100

    Basic interface working.

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
